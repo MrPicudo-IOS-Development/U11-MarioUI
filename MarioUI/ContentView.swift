5e53ff84ea9c2b96e37aa68b4540636c8556a302
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-02-25 17:15:09 -0600

    Proyecto terminado
    Este fue un proyecto súper sencillo para crear una "I am rich App" con Swift UI, pero se le agregó una imagen vectorizada desde Canva, y diseños propios para mejorar la experiencia y la calidad de la app.
    Se trabajaron con los objetos y sus modificadores desde la interfaz de Swift UI, pero hay mucho más por entender y aplicar.

--- MarioUI/ContentView.swift
+++ MarioUI/ContentView.swift
@@ -1,22 +1,32 @@
 /* ContentView.swift --> MarioUI. Created by Miguel Torres on 25/02/23. */
 
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 84/255, green: 180/255, blue: 53/255)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("Mario Image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("It's me, Mario!")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                Image(systemName: "bolt.heart")
+                    .resizable()
+                    .frame(width: 93, height: 93)
+                    .imageScale(.large)
+                    .foregroundColor(.white)
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView() // Esto carga el dispositivo por default del preview (es el que tenemos seleccionado en el simulador)
     }
 }
