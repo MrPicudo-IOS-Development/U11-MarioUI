49279768df007574f8d3ad2e5e3452b1f4bda642
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-02-25 12:13:42 -0600

    Creación del proyecto
    Después de más de 20 días, la aplicación de Clima terminada con consultas a una API, y la lección de la terminal completada, empiezo a estudiar Swift UI para mi curso del lunes 27 de febrero.

---
+++ MarioUI/ContentView.swift
@@ -0,0 +1,22 @@
+/* ContentView.swift --> MarioUI. Created by Miguel Torres on 25/02/23. */
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarioUI/MarioUIApp.swift
@@ -0,0 +1,12 @@
+/* MarioUIApp.swift --> MarioUI. Created by Miguel Torres on 25/02/23. */
+
+import SwiftUI
+
+@main
+struct MarioUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
